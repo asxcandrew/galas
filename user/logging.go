c522ea6056e91156f198923f528488a367ec322e
Andrey Skoryk <asxcandrew@gmail.com>
2019-04-07 15:47:27 +0300

    Logging for auth actions

--- user/logging.go
+++ user/logging.go
@@ -1,7 +1,11 @@
 package user
 
 import (
+	"time"
+
 	"github.com/go-kit/kit/log"
+
+	"github.com/asxcandrew/galas/storage/model"
 )
 
 type userLoggingService struct {
@@ -11,5 +15,31 @@ type userLoggingService struct {
 
 // NewLoggingService returns a new instance of a itemLoggingService.
 func NewUserLoggingService(logger log.Logger, s UserService) UserService {
+	logger = log.With(logger, "service", "users")
+
 	return &userLoggingService{logger, s}
+}
+
+func (s *userLoggingService) Login(email, password string) (user *model.User, err error) {
+	defer func(begin time.Time) {
+		s.logger.Log(
+			"method", "login",
+			"email", email,
+			"took", time.Since(begin),
+			"err", err,
+		)
+	}(time.Now())
+	return s.UserService.Login(email, password)
+}
+
+func (s *userLoggingService) Register(user *model.User, password string) (err error) {
+	defer func(begin time.Time) {
+		s.logger.Log(
+			"method", "register",
+			"email", user.Email,
+			"took", time.Since(begin),
+			"err", err,
+		)
+	}(time.Now())
+	return s.UserService.Register(user, password)
 }
